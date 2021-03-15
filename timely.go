82c43be4f5b527be475c7fa21c1ad401b9fa26f7
dzon2000 <dzon2000@protonmail.com>
2021-03-15 14:11:08 +0100

    Added aggregation for TAG and filtering by TAG

--- timely.go
+++ timely.go
@@ -7,6 +7,7 @@ import (
     "github.com/dzon2000/timely/io"
     "github.com/dzon2000/timely/data"
     "github.com/dzon2000/timely/color"
+    "github.com/dzon2000/timely/format"
 )
 
 const PROG_NAME = "timely"
@@ -22,6 +23,14 @@ func list() {
     }
 }
 
+func listTag(tag string) {
+    jobs := io.Read()
+    for _, job := range jobs {
+        if job.Tag == tag {
+            fmt.Println(job)
+        }
+    }
+}
 
 func stop() {
     jobs := io.Read()
@@ -46,6 +55,28 @@ func stop() {
         return
     }
     io.Write(jobs)
+}
+
+func aggregate() {
+    jobs := io.Read()
+    byTag := make(map[string][]data.Job)
+    for _, job := range jobs {
+        val, ok := byTag[job.Tag]
+        if ok {
+            val = append(val, job)
+            byTag[job.Tag] = val
+        } else {
+            byTag[job.Tag] = []data.Job{job}
+        }
+    }
+    for k := range byTag {
+        jobs := byTag[k]
+        tTime := int64(0)
+        for _, job := range jobs {
+            tTime += job.Time
+        }
+        fmt.Printf("%s - %s\n", k, format.FormatSec(tTime))
+    }
 }
 
 func start(tag, desc string) {
@@ -74,12 +105,20 @@ func main() {
         fmt.Println("Creating the file...")
         io.Init()
     case "list":
-        fmt.Println("Listing......")
-        list()
+        if len(args) == 1 {
+            fmt.Println("Listing all")
+            list()
+        } else {
+            fmt.Printf("Listing for %s\n", args[1])
+            listTag(args[1])
+        }
     case "start":
         fmt.Printf("Adding %s to %s\n", args[2], args[1])
         start(args[1], args[2])
     case "stop":
         stop()
+    case "aggr":
+        fmt.Println("Listing aggregated view")
+        aggregate()
     }
 }
